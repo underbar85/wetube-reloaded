f47baaf063e2bc5845349a18ab418bf4c3c44003
DK Kim <codker@outlook.kr>
2022-12-18 18:18:52 +0900

    dos: express - app의 .get과 .use에 대해서 공부함
    - .get을 통해서 reqest와 response에 대해서 이해하였고
    - .use를 통해서 middleware의 사용 방법, global middleware에 대한 개념을 이해하게 되었음

--- src/server.js
+++ src/server.js
@@ -1,8 +1,26 @@
-import express from "express";
+import express from "express"
 
 const port = 4000
-const app = express();
+const app = express()
 
-const handleListening = () => console.log( `Server listening on port http://localhost:${port} 🚀`)
+const logger = (req, res, next) => {
+	console.log(`${req.method} ${req.url}`)
+	next()
+}
+
+const handleHome = (req, res) => {
+	return res.send("I love middlewares")
+}
+
+const handleLogin = (req, res) => {
+    return res.send("Login here.")
+};
+
+
+const handleListening = () => 
+console.log( `Server listening on port http://localhost:${port} 🚀`)
+
+app.get("/",logger, handleHome)
+app.get("/login", handleLogin)
 
 app.listen(port, handleListening)
