4811e62f31a9ff3420368a0825f5d158344811fd
DK Kim <codker@outlook.kr>
2022-12-19 18:34:16 +0900

    docs: Router 생성
    - app.use(), app.get()을 사용해서 Router를 만들어보았다
    - 먼저 app.get()을이용하여 각 페이지 별 정의를 해주고, 아래 app.use()를 이용하요 대표 도메인들을 만들었다

--- src/server.js
+++ src/server.js
@@ -1,26 +1,42 @@
 import express from "express"
+import morgan from "morgan"
 
-const port = 4000
-const app = express()
+const app = express();
+const logger = morgan("dev");
+app.use(logger)
 
-const logger = (req, res, next) => {
-	console.log(`${req.method} ${req.url}`)
-	next()
-}
+const port = 3000
 
-const handleHome = (req, res) => {
-	return res.send("I love middlewares")
-}
+const globalRouter = express.Router()
+const handleHome = (req, res) => res.send("Home")
+globalRouter.get("/", handleHome)
 
-const handleLogin = (req, res) => {
-    return res.send("Login here.")
-};
+const videoRouter = express.Router()
+const handleWatchVideo = (req, res) => res.send("Watch Video")
+videoRouter.get("/watch", handleWatchVideo)
 
+const userRouter = express.Router()
+const handleEditUser = (req, res) => res.send("Edit User")
+userRouter.get("/edit", handleEditUser)
+
+
+app.use("/", globalRouter)
+app.use("/videos", videoRouter)
+app.use("/users", userRouter)
+
+// const home = (req, res) => {
+// 	console.log("I will respond")
+// 	return res.send("hello")
+// }
+
+// const login = (req, res) => {
+//     return res.send("login")
+// };
+
+// app.get("/", home)
+// app.get("/login", login)
 
 const handleListening = () => 
-console.log( `Server listening on port http://localhost:${port} 🚀`)
-
-app.get("/",logger, handleHome)
-app.get("/login", handleLogin)
+	console.log( `Server listening on port http://localhost:${port} 🚀`)
 
 app.listen(port, handleListening)
