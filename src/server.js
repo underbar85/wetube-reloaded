bae2282fcf7e1c996499ba6937b29147afb849bc
DK Kim <codker@outlook.kr>
2022-12-19 23:27:27 +0900

    docs: Added a folder of router and router's js files
    - 깔끔한 코드 작성을 위하여, 라우터를 server.js에서 분리
    - 분리를 위하여 src 안에 routers 폴더를 생성하고, globalRouter, userRouter, videoRouter 각각 js파일을 생성하여 옮

--- src/server.js
+++ src/server.js
@@ -1,40 +1,18 @@
 import express from "express"
 import morgan from "morgan"
+import globalRouter from "./routers/globalRouter"
+import userRouter from "./routers/userRouter"
+import videoRouter from "./routers/videoRouter"
 
 const app = express();
 const logger = morgan("dev");
 app.use(logger)
 
-const port = 3000
-
-const globalRouter = express.Router()
-const handleHome = (req, res) => res.send("Home")
-globalRouter.get("/", handleHome)
-
-const videoRouter = express.Router()
-const handleWatchVideo = (req, res) => res.send("Watch Video")
-videoRouter.get("/watch", handleWatchVideo)
-
-const userRouter = express.Router()
-const handleEditUser = (req, res) => res.send("Edit User")
-userRouter.get("/edit", handleEditUser)
-
+const port = 4000
 
 app.use("/", globalRouter)
 app.use("/videos", videoRouter)
 app.use("/users", userRouter)
-
-// const home = (req, res) => {
-// 	console.log("I will respond")
-// 	return res.send("hello")
-// }
-
-// const login = (req, res) => {
-//     return res.send("login")
-// };
-
-// app.get("/", home)
-// app.get("/login", login)
 
 const handleListening = () => 
 	console.log( `Server listening on port http://localhost:${port} 🚀`)
