06e2864829473bba2ae752f5fe09b08b384066f3
DK Kim <codker@outlook.kr>
2023-01-16 23:28:55 +0900

    fix: video router, video controller 파일 수정 - route - see 누락되어 추가후 다시 커밋

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => res.send("Home");
-export const watch = (req, res) => res.send("Watch");
+export const see = (req, res) => res.send("See Video");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { see, edit } from "../controllers/videoController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
