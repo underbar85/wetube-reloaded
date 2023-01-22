a3097ec8a3038a395adb39ddbdc7a2ea5e62d8ff
DK Kim <codker@outlook.kr>
2023-01-22 23:05:19 +0900

    build: URL parameters 추가 - URL 파라미터 이해 - req.params에 대한 이해해

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,6 +6,6 @@ const videoRouter = express.Router();
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id", see);
 videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete" deleteVideo);
+videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
