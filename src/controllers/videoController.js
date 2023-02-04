d9f4b32356663b7ab6415ba4fc8b3f3c2197afe8
DK Kim <codker@outlook.kr>
2023-02-05 03:11:46 +0900

    docs: pug > partial을 이용하여 템플릿 작업을 시작 - pug를 이용하여 html을 작성 - pug의 include를 이용하여 반복 작업을 템플릿화 진행

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,18 +1,8 @@
-export const trending = (req, res) =>
-  res.send(
-    "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>"
-  );
+export const trending = (req, res) => res.render("home");
 
-export const see = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-  );
-};
+export const see = (req, res) => res.render("watch");
 
-export const edit = (req, res) =>
-  res.send(
-    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-  );
+export const edit = (req, res) => res.render("edit");
 
 export const search = (req, res) => res.send("Search");
 
--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ import videoRouter from "./routers/videoRouter";
 
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
 const port = 4000;
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
